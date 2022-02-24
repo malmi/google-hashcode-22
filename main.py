bb301c44bfb8d805fefe5a925d7ed2546abec18a
Michael Malcharek <michael.malcharek@evoleen.com>
2022-02-24 18:54:11 +0100

    Push input files

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 def main():
+    arik()
+    ramon()
+    alicia()
+    michael()
+    pass
+
+
+def arik():
+    print("lolchopf")
+    pass
+
+
+def ramon():
+    print("ron weasley")
+    pass
+
+
+def alicia():
+    print("Hermine")
+    pass
+
+
+def michael():
+    N_contributers = []
+    M_projects = []
+
+    print("hi you")
     pass
 
 
